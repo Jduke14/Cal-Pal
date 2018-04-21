3736ffedc949b70e25c13e66063cf230fa61bab1
Unknown <jduke14@lsu.edu>
2018-04-21 13:33:35 -0500

    Fixed login errors
    Updated security.yaml, SecurityContoller.php, and login.html.twig to ensure properly working login

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,15 +3,16 @@ namespace App\Controller;
 
 use App\Form\LoginForm;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends Controller
 {
     /**
-     * @Route("/login", name="security_login")
+     * @Route("/login", name="login")
      */
-    public function loginAction(Request $request, AuthenticationUtils $authenticationUtils)
+    public function login(Request $request, AuthenticationUtils $authenticationUtils)
     {
        
         // get the login error if there is one
@@ -24,13 +25,11 @@ class SecurityController extends Controller
             '_username' => $lastUsername,
         ]);
 
-        return $this->render(
-            'security/login.html.twig',
-            array(
+        return $this->render('security/login.html.twig', array(
                 // last username entered by the user
                 'last_username' => $lastUsername,
                 'error'         => $error,
-            )
-        );
+            
+        ));
     }
 }
